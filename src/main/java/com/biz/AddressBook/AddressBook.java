7ee11710112d44bfb9557cb3c418e9a216893e12
rishikaran0207 <sendittomemrk@gmail.com>
2021-09-29 18:41:16 +0530

    Added: Sorting in alphabetical order

---
+++ src/main/java/com/biz/AddressBook/AddressBook.java
@@ -0,0 +1,6 @@
+package com.biz.AddressBook;
+import java.util.HashMap;
+import java.util.Map;
+public class AddressBook {
+    public static Map<String, Contact> book = new HashMap<String, Contact>();
+}
